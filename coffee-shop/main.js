935306526a79ea102441b59dd1bd64809bff5f8b
reinheimermat <mreinheimer0@gmail.com>
2023-10-25 11:24:42 -0300

    finished-project

--- coffee-shop/main.js
+++ coffee-shop/main.js
@@ -1,4 +1,10 @@
 import './src/style.scss'
-
-
 // document.querySelector("#app").innerHTML = ``
+
+const inputCheck = document.querySelector('#modo-noturno')
+const elemento = document.querySelector('body')
+
+inputCheck.addEventListener('click', _ => {
+    const modo = inputCheck.checked ? 'dark' : 'light'
+    elemento.setAttribute("data-bs-theme", modo)
+})
